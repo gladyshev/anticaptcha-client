035dbfd7977d1cb466bb6a1889b03f153ac0eb40
Dmitry Gladyshev <deel@email.ru>
2017-09-11 10:55:19 +0300

    Update queue_stats.php

--- examples/queue_stats.php
+++ examples/queue_stats.php
@@ -7,6 +7,6 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $client = new \Anticaptcha\Client(getenv('__ANTICAPTCHA_KEY__'));
 
-$balance = $client->getQueueStats(\Anticaptcha\Entity\QueueStats::QUEUE_IMAGE_TO_TEXT_RU);
+$stats = $client->getQueueStats(\Anticaptcha\Entity\QueueStats::QUEUE_IMAGE_TO_TEXT_RU);
 
-var_dump($balance);
+var_dump($stats);
